cfb4e327eab61bae73d10cdb671a8feff3a04894
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-08-19 13:35:47 +0900

    フォームを作成

---
+++ web/modules/custom/marucha/src/Form/MaruchaForm.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\marucha\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implementing a simple form.
+ */
+class MaruchaForm extends FormBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    return 'marucha_form';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your name please.'),
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addStatus($this->t('Your name is @name', ['@name' => $form_state->getValue('name')]));
+  }
+
+}
