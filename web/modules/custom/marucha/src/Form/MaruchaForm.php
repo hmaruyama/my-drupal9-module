9b8703244be078a1c6e291e2c050d56ad1368978
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-09-21 13:25:40 +0900

    さまざまなフォーム要素の追加

--- web/modules/custom/marucha/src/Form/MaruchaForm.php
+++ web/modules/custom/marucha/src/Form/MaruchaForm.php
@@ -25,6 +25,43 @@ class MaruchaForm extends FormBase {
       '#type' => 'textfield',
       '#title' => '名前',
     ];
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => 'メールアドレス',
+      '#pattern' => '*@example.com',
+    ];
+    $form['phone'] = [
+      '#type' => 'tel',
+      '#title' => '電話番号',
+      '#pattern' => '[^\\d]*',
+    ];
+    $form['radios'] = [
+      '#type' => 'radios',
+      '#title' => 'ラジオボタン',
+      '#options' => [
+        0 => '選択A',
+        1 => '選択B',
+      ],
+    ];
+    $form['checkbox'] = [
+      '#type' => 'checkbox',
+      '#title' => 'チェックボックス',
+    ];
+    $form['select'] = [
+      '#type' => 'select',
+      '#title' => '選択',
+      '#options' => [
+        0 => '選択A',
+        1 => '選択B',
+        2 => '選択C',
+      ],
+      '#default_value' => 1,
+    ];
+    $form['color'] = [
+      '#type' => 'color',
+      '#title' => 'カラー',
+      '#default_value' => '#ff0000',
+    ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => '送信！',
