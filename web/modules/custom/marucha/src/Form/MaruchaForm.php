8e99fb40a56cac9db6029955dce14b36e8495fe0
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-10-04 13:10:55 +0900

    サービスを利用する(依存性の注入)

--- web/modules/custom/marucha/src/Form/MaruchaForm.php
+++ web/modules/custom/marucha/src/Form/MaruchaForm.php
@@ -4,11 +4,39 @@ namespace Drupal\marucha\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Component\Utility\EmailValidator;
 
 /**
  * Provide a simple form.
  */
 class MaruchaForm extends FormBase {
+
+  /**
+   * An email validator.
+   *
+   * @var \Drupal\Component\Utility\EmailValidator
+   */
+  protected $emailValidator;
+
+  /**
+   * Constructs.
+   *
+   * @param \Drupal\Component\Utility\EmailValidator $email_validator
+   *   This is an email validator service.
+   */
+  public function __construct(EmailValidator $email_validator) {
+    $this->emailValidator = $email_validator;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('email.validator')
+    );
+  }
 
   /**
    * {@inheritDoc}
@@ -37,7 +65,8 @@ class MaruchaForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $email = $form_state->getValue('email');
-    $flag = \Drupal::service('email.validator')->isValid($email);
+    // $flag = \Drupal::service('email.validator')->isValid($email);
+    $flag = $this->emailValidator->isValid($email);
     if (!$flag) {
       $form_state->setErrorByName('email', 'メールアドレスの形式が正しくありません');
     }
