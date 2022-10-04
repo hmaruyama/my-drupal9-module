6a615b0aadbade956d5626c146b83d9642e22856
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-10-04 15:50:58 +0900

    独自のサービスをフォームクラスに注入

--- web/modules/custom/marucha/src/Form/MaruchaForm.php
+++ web/modules/custom/marucha/src/Form/MaruchaForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Component\Utility\EmailValidator;
+use Drupal\marucha\Service\MaruchaService;
 
 /**
  * Provide a simple form.
@@ -20,13 +21,23 @@ class MaruchaForm extends FormBase {
   protected $emailValidator;
 
   /**
+   * An marucha service.
+   *
+   * @var \Drupal\marucha\Service\MaruchaService
+   */
+  protected $maruchaService;
+
+  /**
    * Constructs.
    *
    * @param \Drupal\Component\Utility\EmailValidator $email_validator
    *   This is an email validator service.
+   * @param \Drupal\marucha\Service\MaruchaService $marucha_service
+   *   This is an email validator service.
    */
-  public function __construct(EmailValidator $email_validator) {
+  public function __construct(EmailValidator $email_validator, MaruchaService $marucha_service) {
     $this->emailValidator = $email_validator;
+    $this->maruchaService = $marucha_service;
   }
 
   /**
@@ -34,7 +45,8 @@ class MaruchaForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('email.validator')
+      $container->get('email.validator'),
+      $container->get('marucha.drink'),
     );
   }
 
@@ -65,7 +77,6 @@ class MaruchaForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $email = $form_state->getValue('email');
-    // $flag = \Drupal::service('email.validator')->isValid($email);
     $flag = $this->emailValidator->isValid($email);
     if (!$flag) {
       $form_state->setErrorByName('email', 'メールアドレスの形式が正しくありません');
@@ -76,7 +87,8 @@ class MaruchaForm extends FormBase {
    * {@inheritDoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus('正常に送信されました！');
+    $message = 'ドリンク占い：' . $this->maruchaService->getDrink();
+    $this->messenger()->addStatus($message);
   }
 
 }
