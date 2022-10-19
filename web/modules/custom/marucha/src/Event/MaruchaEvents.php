a6a1d67999a8f8576ab2c6a452912f59c0bc6656
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-10-19 13:36:15 +0900

    イベントに一意の名前を付ける

---
+++ web/modules/custom/marucha/src/Event/MaruchaEvents.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\marucha\Event;
+
+/**
+ * Defines marucha's events.
+ */
+class MaruchaEvents {
+
+  /**
+   * Marucha's fist event.
+   *
+   * @Event
+   *
+   * @see Drupal\marucha\Event\MaruchaFirstEvent
+   */
+  const FIRST_EVENT = 'marucha.first_event';
+
+}
